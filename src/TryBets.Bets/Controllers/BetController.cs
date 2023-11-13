a96e49432c30a673aa840c1b6eea4c854189c4f5
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-11-13 10:00:53 -0300

    req 3

--- src/TryBets.Bets/Controllers/BetController.cs
+++ src/TryBets.Bets/Controllers/BetController.cs
@@ -20,13 +20,21 @@ public class BetController : Controller
         _repository = repository;
         _oddService = oddService;
     }
-
     [HttpPost]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Authorize(Policy = "Client")]
-    public async Task<IActionResult> Post([FromBody] BetDTORequest request)
+    public IActionResult Post([FromBody] BetDTORequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var token = HttpContext.User.Identity as ClaimsIdentity;
+            var email = token?.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            return Created("", _repository.Post(request, email!));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 
     [HttpGet("{BetId}")]
@@ -34,6 +42,15 @@ public class BetController : Controller
     [Authorize(Policy = "Client")]
     public IActionResult Get(int BetId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var token = HttpContext.User.Identity as ClaimsIdentity;
+            var email = token?.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            return Ok(_repository.Get(BetId, email!));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 }
